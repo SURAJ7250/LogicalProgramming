5e47f35b4c5237c08a361527065914bf7ad5f276
surajbridgelabz <surajnsinha@gmail.com>
2022-08-28 20:40:09 +0530

    [suraj] Add: perfect number

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -8,13 +8,17 @@ namespace LogicalProgram
             Console.WriteLine("Welcome to logical programming");
             while (true)
             {
-                Console.WriteLine("Select program\n 1. fibonacci series");
+                Console.WriteLine("Select program\n 1. fibonacci series\n 2. perfect number");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         FibonacciSeries fibonacciSeries = new FibonacciSeries();
                         fibonacciSeries.Series();
+                        break;
+                    case 2:
+                        PerfectNumber perfectNumber = new PerfectNumber();
+                        perfectNumber.Number();
                         break;
                     default:
                         Console.WriteLine("Select correct option");
